5a74a508335b841ae218c39211682736a2cce660
Balázs Begov <bbegov@googlemail.com>
2022-08-12 21:23:36 +0200

    Hashing added as service to DI

--- leave-a-note-core/Program.cs
+++ leave-a-note-core/Program.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+using leave_a_note_core.Services.PasswordHasher;
 using leave_a_note_data;
 using leave_a_note_data.Entities;
 using leave_a_note_data.Repositories;
@@ -6,7 +8,12 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(opt => opt.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 // Add data storage service
 builder.Services.AddDbContext<LeaveANoteDbContext>(options =>
@@ -17,9 +24,8 @@ builder.Services.AddTransient<DataSeeder>();
 builder.Services.AddTransient<IRepository<User>, UserRepository>();
 builder.Services.AddTransient<IRepository<Note>, NoteRepository>();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Add password hashing service
+builder.Services.AddTransient<IPasswordHasher, BCryptPasswordHasher>();
 
 var app = builder.Build();
 
