92826552dde06207a46c4dcae67acb97a3d5cea1
Balázs Begov <bbegov@googlemail.com>
2022-08-12 13:53:14 +0200

    Entities created

---
+++ leave-a-note-data/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace leave_a_note_data.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        
+        [Required]
+        public string LastName { get; set; }
+        
+        [Required]
+        public string PasswrodHash { get; set; }
+        
+        [Required]
+        public UserRole role { get; set; }
+    }
+}
---
+++ leave-a-note-data/Entities/UserRole.cs
@@ -0,0 +1,7 @@
+﻿namespace leave_a_note_data.Entities;
+
+public enum UserRole
+{
+    Admin,
+    User
+}
