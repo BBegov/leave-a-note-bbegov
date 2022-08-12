79c8223dd14a02dd1f6536e15e584cd05e80ff1b
Balázs Begov <bbegov@googlemail.com>
2022-08-12 21:23:05 +0200

    Password hasher created for hashing and check

---
+++ leave-a-note-core/Services/PasswordHasher/IPasswordHasher.cs
@@ -0,0 +1,8 @@
+﻿namespace leave_a_note_core.Services.PasswordHasher
+{
+    public interface IPasswordHasher
+    {
+        string HashPassword(string password);
+        bool VerifyPassword(string password, string passwordHash);
+    }
+}
