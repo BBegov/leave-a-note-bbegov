8d0b707e95561c6504a8d5c67566f72f0dafd6d2
Balázs Begov <bbegov@googlemail.com>
2022-08-12 17:06:01 +0200

    Interface added for the repositories

---
+++ leave-a-note-data/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace leave_a_note_data.Repositories
+{
+    public interface IRepository<T>
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T> GetAsync(int id);
+        Task<T> UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+        Task AddAsync(T entity);
+    }
+}
