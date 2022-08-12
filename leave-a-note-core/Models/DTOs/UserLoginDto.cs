dc180c1c88edf2e8bbca1a06be4c2c6e123b4fee
Balázs Begov <bbegov@googlemail.com>
2022-08-12 21:22:31 +0200

    DTOs created to transform entities there and back

---
+++ leave-a-note-core/Models/DTOs/UserLoginDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using leave_a_note_data.Entities;
+
+namespace leave_a_note_core.Models.DTOs;
+
+public class UserLoginDto
+{
+    [Required]
+    public int Id { get; set; }
+    
+    [Required]
+    public string UserName { get; set; }
+    
+    [Required]
+    public string PasswordHash { get; set; }
+    
+    [Required]
+    public UserRole Role { get; set; }
+}
---
+++ leave-a-note-core/Models/DTOs/UserUpdateDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace leave_a_note_core.Models.DTOs;
+
+public class UserUpdateDto
+{
+    [Required]
+    [StringLength(32, MinimumLength = 2, ErrorMessage = "First name must be between 2 and 32 characters long")]
+    public string FirstName { get; set; }
+
+    [Required]
+    [StringLength(32, MinimumLength = 2, ErrorMessage = "Last name must be between 2 and 32 characters long")]
+    public string LastName { get; set; }
+
+    [Required]
+    [StringLength(32, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 32 characters long")]
+    public string Password { get; set; }
+}
