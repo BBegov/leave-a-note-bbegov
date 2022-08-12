810e9278aecded25e44b90b953ed197b16f4166d
Balázs Begov <bbegov@googlemail.com>
2022-08-12 16:49:59 +0200

    DbContext and Data seeder file added as services. Seeding run on startup

--- leave-a-note-core/Program.cs
+++ leave-a-note-core/Program.cs
@@ -1,13 +1,27 @@
+using leave_a_note_data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Data storage service
+builder.Services.AddDbContext<LeaveANoteDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddTransient<DataSeeder>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+var initializer = services.GetRequiredService<DataSeeder>();
+initializer.Seed();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
