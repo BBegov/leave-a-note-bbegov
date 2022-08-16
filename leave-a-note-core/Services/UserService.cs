e158e81026ab55e73f8c75dc65b55a8e2e92db40
Balázs Begov <bbegov@googlemail.com>
2022-08-16 11:57:45 +0200

    Password hasher added to user service and changing password implemented

--- leave-a-note-core/Services/UserService.cs
+++ leave-a-note-core/Services/UserService.cs
@@ -1,17 +1,20 @@
-﻿using leave_a_note_core.Extensions;
+﻿using BCrypt.Net;
+using leave_a_note_core.Extensions;
 using leave_a_note_core.Models.DTOs;
-using leave_a_note_data.Entities;
+using leave_a_note_core.Services.PasswordHasher;
 using leave_a_note_data.Repositories;
 
 namespace leave_a_note_core.Services
 {
     public class UserService : IUserService
     {
-        private readonly IRepository<User> _userRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly IPasswordHasher _passwordHasher;
 
-        public UserService(IRepository<User> userRepository)
+        public UserService(IUserRepository userRepository, IPasswordHasher passwordHasher)
         {
             _userRepository = userRepository;
+            _passwordHasher = passwordHasher;
         }
 
         public async Task<List<UserViewDto>> GetAllUsersAsync()
@@ -29,6 +32,7 @@ namespace leave_a_note_core.Services
 
         public async Task<UserViewDto> AddUserAsync(UserCreateDto userCreateDto)
         {
+            userCreateDto.Password = _passwordHasher.HashPassword(userCreateDto.Password);
             var newUser = userCreateDto.ToUserEntity();
             await _userRepository.AddAsync(newUser);
             return newUser.ToUserViewDto();
@@ -43,6 +47,20 @@ namespace leave_a_note_core.Services
         public async Task DeleteUserAsync(int id)
         {
             await _userRepository.DeleteAsync(id);
+        }
+
+        public async Task<UserViewDto> ChangePassword(UserChangePasswordDto userChangePasswordDto)
+        {
+            var userToUpdate = (await _userRepository.GetAsync(userChangePasswordDto.Id)).ToUserPasswordUpdateDto();
+            
+            if (!_passwordHasher.VerifyPassword(userChangePasswordDto.OldPassword, userToUpdate.Password))
+            {
+                throw new BcryptAuthenticationException();
+            }
+
+            userToUpdate.Password = _passwordHasher.HashPassword(userChangePasswordDto.NewPassword);
+
+            return (await _userRepository.UpdateWithPasswordAsync(userToUpdate.ToUserEntity())).ToUserViewDto();
         }
     }
 }
