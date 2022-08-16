1ccb67bc6c7cf4d2bb92a98da520cb7ebcc19361
Balázs Begov <bbegov@googlemail.com>
2022-08-16 13:47:38 +0200

    Refactoring simplify namespace definition

--- leave-a-note-core/Services/IUserService.cs
+++ leave-a-note-core/Services/IUserService.cs
@@ -1,15 +1,14 @@
 ﻿using leave_a_note_core.Models.DTOs;
 
-namespace leave_a_note_core.Services
-{
-    public interface IUserService
-    {
-        Task<List<UserViewDto>> GetAllUsersAsync();
-        Task<UserViewDto> GetUserByIdAsync(int id);
-        Task<UserViewDto> AddUserAsync(UserCreateDto userCreateDto);
-        Task<UserViewDto> UpdateUserAsync(UserUpdateDto userUpdateDto);
-        Task DeleteUserAsync(int id);
-        Task<UserViewDto> ChangePassword(UserChangePasswordDto userChangePasswordDto);
+namespace leave_a_note_core.Services;
 
-    }
+public interface IUserService
+{
+    Task<List<UserViewDto>> GetAllUsersAsync();
+    Task<UserViewDto> GetUserByIdAsync(int id);
+    Task<UserViewDto> AddUserAsync(UserCreateDto userCreateDto);
+    Task<UserViewDto> UpdateUserAsync(UserUpdateDto userUpdateDto);
+    Task DeleteUserAsync(int id);
+    Task<UserViewDto> ChangePassword(UserChangePasswordDto userChangePasswordDto);
+
 }
