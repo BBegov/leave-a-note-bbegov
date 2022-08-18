3ce0bf6e9bd58972611d4eff65dbd87af2ea4d38
Balázs Begov <bbegov@googlemail.com>
2022-08-18 12:46:41 +0200

    Added navbar to app.js

--- leave-a-note-client/src/App.js
+++ leave-a-note-client/src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
+import AppRouter from './components/AppRouter';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Landing page</p>
-      </header>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="/home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="">Home</Nav.Link>
+              <Nav.Link href="mynotes">My Notes</Nav.Link>
+              <Nav.Link href="login">Login</Nav.Link>
+              <Nav.Link href="users">Users</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <AppRouter />
     </div>
   );
 }
