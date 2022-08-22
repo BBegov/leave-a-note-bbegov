a2703bb18339133c7695c9fa72921403ada9b3e9
Balázs Begov <bbegov@googlemail.com>
2022-08-22 16:17:40 +0200

    Routes moved into index.js

--- leave-a-note-client/src/index.js
+++ leave-a-note-client/src/index.js
@@ -2,10 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Notes from './components/Notes';
+import Users from './components/Users';
+import FlashMessageProvider from './context/FlashMessageProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <FlashMessageProvider>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="home" element={<Notes url={"https://localhost:44321/api/Notes"} />} />
+            <Route path="mynotes" element={<Notes url={"https://localhost:44321/api/Users/1/Notes"} />} />
+            <Route path="users" element={<Users url={"https://localhost:44321/api/Users"} />} />
+          </Route>
+        </Routes>
+      </FlashMessageProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
