6bf2b26230229b9dda1e5004ee45d93496be8df3
Balázs Begov <bbegov@googlemail.com>
2022-08-22 16:16:42 +0200

    Outlet and header added

--- leave-a-note-client/src/App.js
+++ leave-a-note-client/src/App.js
@@ -1,28 +1,36 @@
 import './App.css';
-import AppRouter from './components/AppRouter';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Brand href="/home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="">Home</Nav.Link>
-              <Nav.Link href="mynotes">My Notes</Nav.Link>
-              <Nav.Link href="login">Login</Nav.Link>
-              <Nav.Link href="users">Users</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <AppRouter />
+      <header className='appHeader'>
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand href="/home">Leave a Note</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="home">Home of Notes</Nav.Link>
+                <Nav.Link href="mynotes">My Notes</Nav.Link>
+                <Nav.Link href="users">Users</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+            <Navbar.Collapse className="justify-content-end">
+              <Navbar.Text>
+                Signed in as: <a href="#login">placeholder</a>
+              </Navbar.Text>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
+      <content className="content">
+        <Outlet />
+      </content>
     </div>
   );
 }
