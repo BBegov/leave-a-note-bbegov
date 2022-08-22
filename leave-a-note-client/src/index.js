e66a460c2fff42d4ef4b17158647d99f591cfc32
Balázs Begov <bbegov@googlemail.com>
2022-08-22 16:21:47 +0200

    Flashmessages context removed from app

--- leave-a-note-client/src/index.js
+++ leave-a-note-client/src/index.js
@@ -5,21 +5,18 @@ import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Notes from './components/Notes';
 import Users from './components/Users';
-import FlashMessageProvider from './context/FlashMessageProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <FlashMessageProvider>
-        <Routes>
-          <Route path="/" element={<App />}>
-            <Route path="home" element={<Notes url={"https://localhost:44321/api/Notes"} />} />
-            <Route path="mynotes" element={<Notes url={"https://localhost:44321/api/Users/1/Notes"} />} />
-            <Route path="users" element={<Users url={"https://localhost:44321/api/Users"} />} />
-          </Route>
-        </Routes>
-      </FlashMessageProvider>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="home" element={<Notes url={"https://localhost:44321/api/Notes"} />} />
+          <Route path="mynotes" element={<Notes url={"https://localhost:44321/api/Users/1/Notes"} />} />
+          <Route path="users" element={<Users url={"https://localhost:44321/api/Users"} />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
