786d3ed504f7fdc89af3c868ede02560c6fef3a1
Balázs Begov <bbegov@googlemail.com>
2022-08-23 23:24:55 +0200

    Created separate main components for App.js

---
+++ leave-a-note-client/src/components/Content.js
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router-dom';
+
+const Content = () => {
+    return (
+        <main className="content">
+            <Outlet />
+        </main>
+    );
+};
+
+export default Content;
