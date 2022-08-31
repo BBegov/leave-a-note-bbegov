432c0586880ae6eb8ad08ba0d0e66737f7975ad3
Balázs Begov <bbegov@googlemail.com>
2022-08-31 20:09:15 +0200

    Request and response classes added

---
+++ leave-a-note-core/Models/Authentication/Requests/LoginRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace leave_a_note_core.Models.Authentication.Requests;
+
+public class LoginRequest
+{
+    [Required]
+    public string Username { get; set; }
+
+    [Required]
+    public string Password { get; set; }
+}
---
+++ leave-a-note-core/Models/Authentication/Responses/AuthenticatedUserResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace leave_a_note_core.Models.Authentication.Responses;
+
+public class AuthenticatedUserResponse
+{
+    public string AccessToken { get; set; }
+}
