5a0b1ebcd8adf111d405afd3524c9c725911795d
Balázs Begov <bbegov@googlemail.com>
2022-08-31 20:08:38 +0200

    Configuration class added for authentication

---
+++ leave-a-note-core/Models/Authentication/AuthenticationConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace leave_a_note_core.Models.Authentication;
+
+public class AuthenticationConfiguration
+{
+    public string AccessTokenSecret { get; set; }
+    public double AccessTokenExpirationMinutes { get; set; }
+    public string Issuer { get; set; }
+    public string Audience { get; set; }
+    public string RefreshTokenSecret { get; set; }
+    public double RefreshTokenExpirationMinutes { get; set; }
+}
