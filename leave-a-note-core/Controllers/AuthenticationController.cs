f5cf3bb05cff2e78f0ebc60ff82547ceba15ce6a
Balázs Begov <bbegov@googlemail.com>
2022-08-31 20:08:10 +0200

    Empty AuthenticationController added

---
+++ leave-a-note-core/Controllers/AuthenticationController.cs
@@ -0,0 +1,28 @@
+﻿using leave_a_note_core.Models.Authentication.Requests;
+using leave_a_note_core.Services;
+using leave_a_note_core.Services.PasswordHasher;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace leave_a_note_core.Controllers;
+
+[Route("api/auth")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+    private readonly IUserService _userService;
+    private readonly IPasswordHasher _passwordHasher;
+
+    public AuthenticationController(IUserService userService, IPasswordHasher passwordHasher)
+    {
+        _userService = userService;
+        _passwordHasher = passwordHasher;
+    }
+
+    [AllowAnonymous]
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginRequest loginRequest)
+    {
+
+    }
+}
